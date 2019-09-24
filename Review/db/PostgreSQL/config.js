e838b511b125a0a2ccc290030f01bb7dd28a9a53
Michael Kim <miketkim@umich.edu>
2019-09-24 15:21:46 -0700

    refactor

--- Review/db/PostgreSQL/config.js
+++ Review/db/PostgreSQL/config.js
@@ -1,11 +1,11 @@
+/* eslint-disable no-unused-vars */
 const { Pool, Client } = require('pg');
 
 const pool = new Pool({
-  user: 'michaelkim',
+  user: 'postgres',
   host: '54.219.177.6',
   database: 'review',
-  password: 'password',
-  port: 5432,
+  password: ''
 });
 
 module.exports = pool;
