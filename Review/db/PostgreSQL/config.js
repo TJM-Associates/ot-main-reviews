19940dac6af50bf4fff09c01d9c69f6891cde6a1
Michael Kim <miketkim@umich.edu>
2019-09-24 15:37:53 -0700

    postgres config

--- Review/db/PostgreSQL/config.js
+++ Review/db/PostgreSQL/config.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
-const { Pool, Client } = require('pg');
+const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'postgres',
+  password: null,
   host: '54.219.177.6',
   database: 'review',
-  password: null,
 });
 
 module.exports = pool;
